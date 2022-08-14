7014e67658d28b8595113bf8c892b251e71e350a
Mahdi Hasan <106563815+Mahdihas@users.noreply.github.com>
2022-08-14 09:08:30 +0600

    Add files via upload

--- explore.js/dom/index.js
+++ explore.js/dom/index.js
@@ -1,29 +1,18 @@
+document.getElementById('btn-delete').addEventListener('click', function(){
+    document.getElementById('secret-info').style.display = "none";
+    
+})
+document.getElementById('delete-confirm').addEventListener('keyup',function(event){
 
-// document.getElementById('button-post').addEventListener('click', function (){
-// const comment = document.getElementById('new-comment');
-// const newComment = comment.value;
-// const container =document.getElementById('container')
-// const p =document.createElement('p');
-// p.innerText = newComment;
-// container.appendChild(p);
-// comment.value = '';
+    const text =event.target.value;
+    const deletebtn = document.getElementById('btn-delete');
+    if(text === 'delete'){
+     deletebtn.removeAttribute('disabled');
 
-// })
-//  document.getElementById('text-field').addEventListener('focus', function(){
-//     console.log('eventriger inside the input field')
-//  })
-//  document.getElementById('text-field').addEventListener('blur', function(){
-//     console.log('eventriger inside the input field')
-//  })
-//  document.getElementById('text-field').addEventListener('keydown', function(event){
-//     console.log(event.target.value)
-//  })
-// document.getElementById('text-field').addEventListener('keypress', function(event){
-//     console.log(event.target.value)
-//  })
-document.getElementById('text-field').addEventListener('keyup', function(event){
-    console.log(event.target.value)
- })
-document.getElementById('btn-event').addEventListener('mousemove' , function(){
-    console.log('evenTriger')
+    }
+    else{
+        deletebtn.setAttribute('disabled',true)
+    }
+
+
 })
